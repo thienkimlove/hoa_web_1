05936ed8f1e85c543e157b649c03fc5e5c1652e7
QuanDo <thienkimlove@gmail.com>
2020-10-06 17:00:33 +0700

    update

---
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,27 @@
+<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i class='nav-icon la la-cat'></i> Chuyên Mục</a></li>
+
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('post') }}'><i class='nav-icon la la-creative-commons'></i> Bài Viết </a></li>
+
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('tag') }}'><i class='nav-icon la la-key'></i> Từ Khóa </a></li>
+
+@if (backpack_user()->hasRole('admin'))
+
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('setting') }}'><i class='nav-icon la la-quora'></i>Settings</a></li>
+
+    <li class="nav-item"><a class="nav-link" href="{{ backpack_url('elfinder') }}\"><i class="nav-icon la la-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
+
+    <!-- Users, Roles, Permissions -->
+    <li class="nav-item nav-dropdown">
+        <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> Authentication</a>
+        <ul class="nav-dropdown-items">
+            <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i> <span>Users</span></a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i class="nav-icon la la-id-badge"></i> <span>Roles</span></a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon la la-key"></i> <span>Permissions</span></a></li>
+        </ul>
+    </li>
+
+
+@endif
