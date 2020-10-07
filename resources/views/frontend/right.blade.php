43580dff427facff19bfaef2f78d4ae16f9ebeba
QuanDo <thienkimlove@gmail.com>
2020-10-07 16:02:46 +0700

    update

--- resources/views/frontend/right.blade.php
+++ resources/views/frontend/right.blade.php
@@ -29,7 +29,10 @@
             <a href="#">Facebook Page</a>
         </h3>
         <div class="Social">
-            <div class="fb-page" data-href="https://www.facebook.com/tuelinh.vn" data-width="100%" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/tuelinh.vn"><a href="https://www.facebook.com/tuelinh.vn">Tuệ Linh</a></blockquote></div></div>
+            <img src="/frontend/imgs/facebook_page.png" />
+            {{--<div class="fb-page" data-href="https://www.facebook.com/tuelinh.vn" data-width="100%" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/tuelinh.vn"><a href="https://www.facebook.com/tuelinh.vn">Tuệ Linh</a></blockquote>--}}
+                {{--</div>--}}
+            {{--</div>--}}
         </div>
     </div>
     <!-- /endSale -->
